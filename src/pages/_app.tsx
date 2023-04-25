76ecfd3b8ad0a2711fb5ea426dd82c1293e8d03d
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-04-25 09:32:17 +0530

    fix: auth, design and ux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import NextProgress from "next-progress";
 import { Toaster } from "react-hot-toast";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
       <Toaster />
       <NextProgress delay={300} options={{ showSpinner: false }} />
-    </>
+    </SessionContextProvider>
   );
 }
--- src/pages/api/save-video-details.ts
+++ src/pages/api/save-video-details.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import getYouTubeID from "get-youtube-id";
 import { Client } from "youtubei";
 import { YoutubeTranscript } from "youtube-transcript";
 import { supabaseClient } from "~/lib/supabase";
@@ -12,32 +11,25 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const { url } = (await req.body) as {
-      url?: string;
+    const { videoId } = (await req.body) as {
+      videoId?: string;
     };
-    if (!url) throw new Error("No url found in req.body");
-
-    const videoId = getYouTubeID(url, { fuzzy: false });
-    if (!videoId) throw new Error("No videoId found");
+    if (!videoId) throw new Error("No videoId found in req.body");
 
     const video = await youtube.getVideo(videoId);
     if (!video) throw new Error("No video found");
 
-    const { title, thumbnails, channel } = video;
+    const { title, thumbnails } = video;
 
     const transcript = await YoutubeTranscript.fetchTranscript(videoId);
 
-    const { error } = await supabaseClient.from("videos").upsert(
-      {
-        url,
-        video_id: videoId,
-        title,
-        thumbnail: thumbnails[thumbnails.length - 1].url,
-        channel_id: channel.id,
-        transcript,
-      },
-      { onConflict: "video_id" }
-    );
+    const { error } = await supabaseClient.from("videos").insert({
+      url: `https://youtu.be/${videoId}`,
+      video_id: videoId,
+      title,
+      thumbnail: thumbnails[thumbnails.length - 1].url,
+      transcript,
+    });
 
     if (error) throw error;
 
