140e226dc06d3746853f3f5470f24a3d2a469f37
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-04-26 19:01:13 +0530

    fix: save summary along with video details

--- src/pages/api/save-video-details.ts
+++ src/pages/api/save-video-details.ts
@@ -2,7 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Client } from "youtubei";
 import { YoutubeTranscript } from "youtube-transcript";
-import { supabaseClient } from "~/lib/supabase";
+import { OpenAI } from "langchain/llms/openai";
+import { loadSummarizationChain } from "langchain/chains";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { convertToText } from "~/utils";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 const youtube = new Client();
 
@@ -11,6 +15,9 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
+    const supabaseClient = createServerSupabaseClient({ req, res });
+    const { data } = await supabaseClient.auth.getUser();
+
     const { videoId } = (await req.body) as {
       videoId?: string;
     };
@@ -23,15 +30,35 @@ export default async function handler(
 
     const transcript = await YoutubeTranscript.fetchTranscript(videoId);
 
+    const text = convertToText(transcript);
+
+    const model = new OpenAI({ temperature: 0 });
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 3000,
+    });
+    const docs = await textSplitter.createDocuments([text]);
+
+    const chain = loadSummarizationChain(model);
+    const summarizeResponse = await chain.call({
+      input_documents: docs,
+    });
+    const summary = summarizeResponse.text;
+
     const { error } = await supabaseClient.from("videos").insert({
       url: `https://youtu.be/${videoId}`,
       video_id: videoId,
       title,
       thumbnail: thumbnails[thumbnails.length - 1].url,
       transcript,
+      summary,
     });
 
     if (error) throw error;
+
+    await supabaseClient.from("history").insert({
+      user_id: data.user?.id,
+      video_id: videoId,
+    });
 
     res.status(200).end();
   } catch (error) {
