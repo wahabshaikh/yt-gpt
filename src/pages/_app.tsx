100e2f4c290f5c4fd1b9cf08ae4d46444066fb90
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-04-30 08:35:27 +0530

    fix: change font to inter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import NextProgress from "next-progress";
 import { Toaster } from "react-hot-toast";
 import { useState } from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
@@ -14,9 +20,11 @@ export default function App({ Component, pageProps }: AppProps) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <Component {...pageProps} />
-      <Toaster />
-      <NextProgress delay={300} options={{ showSpinner: false }} />
+      <div className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+        <Toaster />
+        <NextProgress delay={300} options={{ showSpinner: false }} />
+      </div>
     </SessionContextProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,13 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: false,
