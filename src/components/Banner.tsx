7cb91ddf2a84de5c31a7c9138a5b685a2952c23f
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-04-30 15:32:06 +0530

    fix: link in banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,8 +4,9 @@ import React from "react";
 const Banner = () => {
   return (
     <Link
-      href="/payment"
+      href="https://thebuilderstudio.gumroad.com/l/ytjarvis"
       className="text-center bg-accent text-accent-content text-sm p-1 font-bold"
+      target="_blank"
     >
       Become an early supporter and get lifetime access to all features for $99
     </Link>
